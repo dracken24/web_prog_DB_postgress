b26066f34fc51e00fc270a6d13fa1b5a44b63843
dracken24 <dracken24@gmail.com>
2025-03-26 15:14:38 -0400

    Ajout devoir 02 prog web

---
+++ frontend/aspNetApp/Program.cs
@@ -0,0 +1,33 @@
+using aspNetApp.Components;
+using Microsoft.JSInterop;
+using Microsoft.AspNetCore.Components;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Ajoutez cette ligne AVANT builder.Build()
+builder.WebHost.UseUrls("http://localhost:5182", "https://localhost:7182");
+
+// Add services to the container.
+builder.Services.AddRazorComponents()
+    .AddInteractiveServerComponents();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+
+app.UseAntiforgery();
+
+app.MapStaticAssets();
+app.MapRazorComponents<App>()
+    .AddInteractiveServerRenderMode();
+
+app.Run();
